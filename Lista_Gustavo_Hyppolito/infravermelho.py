054cd7b1cd9c3e6112d8dddfec11200247640b27
Gustavo-Hyppolito <gustavo.h.rodrigues10@aluno.senai.br>
2026-04-29 07:41:16 -0300

    Create infravermelho.py

---
+++ Lista_Gustavo_Hyppolito/infravermelho.py
@@ -0,0 +1,16 @@
+from machine import Pin
+from utime import sleep
+
+
+sensor_presenca = Pin(15, Pin.IN)
+
+while True:
+    leitura_sensor = sensor_presenca.value()
+    
+    if(leitura_sensor == 0):
+        print("Presença detectada")
+        
+    else:
+        print("Nada detectado")
+        
+    sleep(0.5)
